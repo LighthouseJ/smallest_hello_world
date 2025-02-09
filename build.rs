290257e3a4efe548de76fab584acf9d408fdc2bb
Nathan Widmyer <lighthousej@gmail.com>
2025-02-09 09:55:12 -0500

    Dumping file size during build

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::process::Command;
 
 fn main() {
@@ -16,5 +17,14 @@ fn main() {
         .status()
         .expect("Failed to optimize binary with objcopy");
 
+    
+    // Get and print the file size
+    if let Ok(metadata) = fs::metadata(&binary_path) {
+        println!("cargo:warning=Final executable size: {} bytes", metadata.len());
+    } else {
+        println!("cargo:warning=Could not determine binary size.");
+    }
+
     println!("cargo:rerun-if-changed=build.rs");
+
 }
